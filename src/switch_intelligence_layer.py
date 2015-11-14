e3f4b95ddd33a4011e4fdeef968637f15cd63677
Rakesh Karanth <rakeshgk1396@gmail.com>
2015-11-13 20:20:21 -0800

    Creating the method stubs
    Adding the initial method stubs that others can use to develop the Fleet
    code related to Checkpoints 1 and 2

---
+++ src/switch_intelligence_layer.py
@@ -0,0 +1,18 @@
+"""
+This class can be used to accept Administrator votes, validate the configuration along
+with the confidence score that is sent by each Administrator and push the flow rules to the
+switches only if the confidence score exceeds a certain threshold
+"""
+
+class SwitchIntelligenceLayer(object):
+    def __init__(self):
+        pass
+
+    def acceptVotes(self):
+        pass
+
+    def validateConfidenceScore(self):
+        pass
+
+    def pushFlowRules(self):
+        pass
---
+++ src/topology.py
@@ -0,0 +1,14 @@
+"""
+We can use this class to create the two topologies that we need
+These two topologies will be created as objects of the Topology class
+"""
+
+class Topology(object):
+    def __init__(self):
+        pass
+
+    def createTopology(self):
+        pass
+
+    def assignNetworkParameters(self):
+        pass
